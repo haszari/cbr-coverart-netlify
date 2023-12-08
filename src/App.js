6a9146d83183621cf82edb510412a0944311a42b
Haszari <haszari@cartoonbeats.com>
2023-12-08 22:21:29 +1300

    add galley sinking (css hard-code version)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CBR012 from './components/covers/cbr012-coming-on';
+import SinkingGalley from './components/covers/cbrxxx-sinkinggalley';
 import Mivova from './components/covers/cbrxxx-mivova';
 import 'styles/app.scss';
 
 function App() {
   return (
     <>
+      <SinkingGalley />
       <Mivova />
       <CBR012 />
     </>
---
+++ src/components/covers/cbrxxx-sinkinggalley.js
@@ -0,0 +1,15 @@
+
+import 'styles/whole-earth.scss';
+
+export default function Cover() {
+  return (
+    <div class="cover galley-sinking">
+      <div class="artwork">
+        <div class="copy">
+          <div class="artist">Haszari</div>
+          <div class="title">Sinking Galley</div>
+        </div>
+      </div>
+    </div>
+  );
+}
