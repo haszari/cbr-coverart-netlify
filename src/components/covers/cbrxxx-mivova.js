22661baa851d07698febb715cf1e9042bcecfb82
Haszari <haszari@cartoonbeats.com>
2023-12-24 16:18:44 +1300

    refactor mivova image to component & comment out all css: - so can experiment with background image & also make this cover self-contained

--- src/components/covers/cbrxxx-mivova.js
+++ src/components/covers/cbrxxx-mivova.js
@@ -1,10 +1,17 @@
 
 import 'styles/whole-earth.scss';
 
+import image from 'images/mivovawave.jpeg';
+
 export default function Cover() {
+  const background = {
+    backgroundImage: `url("${image}")`,
+  };
+
+
   return (
     <div className="cover mivova">
-      <div className="artwork">
+      <div className="artwork"  style={ background }>
         <div className="copy">
           <div className="artist">Haszari</div>
           <div className="title">Mivova</div>
