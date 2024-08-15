3fc5db0e3dbd79a57d1a3489e15fcd57b0dd674e
Haszari <haszari@cartoonbeats.com>
2024-08-15 17:05:45 +1200

    add dj/{ymlfile} route and component for rendering yml playlists as cover grid + text

---
+++ src/components/djmix/djmix-info.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+
+import YAML from "yaml";
+
+const useShowYaml = (url) => {
+  const [showInfo, setShowInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const raw = await response.text();
+
+        const parsed = YAML.parse(raw);
+
+        setShowInfo(parsed);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+    return () => {};
+  }, [url]);
+
+  return { showInfo, loading, error };
+};
+
+export default function ShowInfo() {
+  let { ymlfile } = useParams();
+
+  const fullFilePath = `${process.env.PUBLIC_URL}/shows/${ymlfile}`;
+  const { showInfo, loading, error } = useShowYaml(fullFilePath);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <img src={showInfo?.tracks[0]?.spotify?.coverArtUrl} />
+      <p>Show info for {showInfo.date} </p>
+    </div>
+  );
+}
